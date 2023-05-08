3866a25e7c72d311bebc49b8d15f9b79addc1e7b
caiodaniel29 <caiosanchez29@gmail.com>
2023-05-08 02:49:14 -0400

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -1,14 +1,52 @@
 /**
- * A class that models a seat.
- *
- * @author Mike, Caio, and Damian
- * @version 5/7/2023
+ * Objects of this class represent seats for a given showing.
+ * It has availability and an alphanumeric designator.
+ * 
+ * @author: Artemis MacDuffie, Ryan Connell, & Tara Nordmann
+ * @version: 5/7/2023
  */
 public class Seat {
+
+    private boolean available;
+    private String number;
+
     /**
-     * Constructor for objects of class Seat
+     * Constructor for objects of class Seat.
+     * @param letter The alphabetic component of the seat number.
+     * @param num The numeric component of the seat number.
      */
-    public Seat() {
+    public Seat(String rowLetter, int num) {
+        number = String.format(rowLetter + "%02d", num);
+        available = true;
+    }
+
+    /**
+     * Returns the seat number, which is made of the row letters and number within that row.
+     * @return The seat number.
+     */
+    public String getNum() {
+        return number;
+    }
+
+    /**
+     * Returns the availability for the seat.
+     * @return The availability for the seat.
+     */
+    public boolean isFree() {
+        return available;
+    }
+
+    /**
+     * Set the seat to unavailable.
+     */
+    public void setUnavailable() {
+        available = false;
+    }
     
+    /**
+     * Set the seat to available.
+     */
+    public void setAvailable() {
+        available = true;
     }
 }
