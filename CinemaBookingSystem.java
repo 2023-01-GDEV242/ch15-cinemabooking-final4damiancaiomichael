f79f7c43f29502402e328e4188df4e97595069df
G00210993 <123398744+G00210993@users.noreply.github.com>
2023-05-08 02:28:34 -0400

    Update CinemaBookingSystem.java

--- CinemaBookingSystem.java
+++ CinemaBookingSystem.java
@@ -6,16 +6,51 @@ import java.util.ArrayList;
  * @version 5/7/2023
  */
 public class CinemaBookingSystem {
-    private ArrayList<Movie> movieList;
+    private ArrayList<Integer> seats = new ArrayList<Integer>();
+    private ArrayList<Customer> customers = new ArrayList<Customer>();
+    private ArrayList<Movie> movies = new ArrayList<Movie>();
+    private ArrayList<Showing> showings = new ArrayList<Showing>();
+    private ArrayList<Theater> theaters = new ArrayList<Theater>();
     /**
      * Constructor for objects of class CinemaBookingSystem
      */
     public CinemaBookingSystem() {
-        
     }
         
     public void testSystem() {
         Movie movie1 = new Movie("The Super Mario Bros. Movie");
-        movieList.add(movie1);
+        movies.add(movie1);
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+    
+    public void removeCustomer(Customer customer) {
+        customers.remove(customer);
+    }    
+    
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+    
+    public void removeMovie(Movie movie) {
+        movies.remove(movie);
+    }
+    
+    public void addShowing(Showing showing) {
+        showings.add(showing);
+    }
+    
+    public void removeShowing(Showing showing) {
+        showings.remove(showing);
+    }    
+    
+    public void addTheater(Theater theater) {
+        theaters.add(theater);
+    }
+    
+    public void removeTheater(Theater theater) {
+        theaters.remove(theater);
     }
 }
